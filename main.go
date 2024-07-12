30ebc81e25ca2a2787370ae36f92c7a58ea23f49
Gabe De Lappe <robertgdel@gmail.com>
2024-07-12 01:13:25 -0700

    Added simple health check endpoint, chirpy logo

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import "net/http"
 
 func main() {
   serverMux := http.NewServeMux()
-  serverMux.Handle("/", http.FileServer(http.Dir(".")))
+  serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./app"))))
+  serverMux.HandleFunc("/healthz", healthCheck)
   server := http.Server {
     Addr: ":8080",
     Handler: serverMux,
   }
   server.ListenAndServe()
+}
+
+func healthCheck(responseWriter http.ResponseWriter, request *http.Request) {
+  responseWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
+  responseWriter.WriteHeader(200)
+  responseWriter.Write([]byte("OK"))
 }
